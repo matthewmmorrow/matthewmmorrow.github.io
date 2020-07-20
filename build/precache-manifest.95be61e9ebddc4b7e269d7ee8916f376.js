be122f904269762289b520a591e32345676e39f4
Matthew Morrow <matt@matthewmmorrow.com>
2020-07-19 23:50:43 -0400

    Initial commit of SciFiCode

---
+++ build/precache-manifest.95be61e9ebddc4b7e269d7ee8916f376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc64443f09057cecf09b17e66541d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8a82164f58a12012195",
+    "url": "/static/css/main.3e4e897d.chunk.css"
+  },
+  {
+    "revision": "544c876ceda847331b13",
+    "url": "/static/js/2.b8a7688f.chunk.js"
+  },
+  {
+    "revision": "e8a82164f58a12012195",
+    "url": "/static/js/main.5b033bdc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
